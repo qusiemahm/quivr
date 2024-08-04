d78fd92cd3625d958a5520414d9c7d9ad87747c2
qusai <qusai.aqrabawe@softylus.com>
2024-08-04 15:27:01 +0300

    update

--- cms/quivr/config/middlewares.js
+++ cms/quivr/config/middlewares.js
@@ -23,6 +23,22 @@ module.exports = [
             'quivr-cms.s3.eu-west-3.amazonaws.com',
             '*.strapi.io',
           ],
+          'style-src': [
+            "'self'",
+            "'unsafe-inline'",
+            'http://localhost:*',
+            'https://preview.quivr.app',
+            'https://*.vercel.app',
+            'https://y040k44.65.21.12.12.sslip.io'
+          ],
+          'script-src': [
+            "'self'",
+            "'unsafe-inline'",
+            "'unsafe-eval'",
+            '*.intercom.io',
+            '*.intercomcdn.com',
+            'https://y040k44.65.21.12.12.sslip.io'
+          ],
           upgradeInsecureRequests: null,
         },
       },
